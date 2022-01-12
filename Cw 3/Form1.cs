c6bd58b9ad542b1e6ab9a21a1695a3f5b406c5d4
Melokawka <84191586+Melokawka@users.noreply.github.com>
2022-01-12 15:05:09 +0100

    Add files via upload

---
+++ Cw 3/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                float x = float.Parse(textBox1.Text);
+                textBox2.Text = (x * x).ToString();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+                MessageBox.Show("Podano błędne dane","Uwaga!",MessageBoxButtons.OK);
+            }
+        }
+
+    }
+}
